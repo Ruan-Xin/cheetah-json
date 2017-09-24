07b03407075bd017902aa53855fd56d58ec72547
SpirtCrazy <415131215@qq.com>
2017-09-24 21:28:04 +0800

    build json

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -19,11 +19,18 @@ import (
 
 var (
 	UnKnownValueTypeError		= errors.New("unknown value type")
+	OutOfBoundsError			= errors.New("out of bounds errors")
+	WrongSymbol					= errors.New("wrong symbol which can't resolve")
 )
 
-type root struct {
-	name string
-	child[] node
+const (
+	NotExist
+	String
+	Number
+)
+
+func ()  {
+	
 }
 
 type node struct {
@@ -31,14 +38,21 @@ type node struct {
 	name string
 	root bool
 	dataType string
-	child[] node
 	key string
+	value_obj node
+	value_arr[] string
+	value string
 }
 
 var pos = 0
-func Parse(input string)  {
-	root := root {
+var length int
+func Parse(input string) error {
+	length = len(input)
+
+	root := node {
 		name:"root",
+		leaf:false,
+		root:true,
 	}
 	var buffer bytes.Buffer
 	var key string
@@ -47,12 +61,66 @@ func Parse(input string)  {
 		if input[pos] == '"' {
 			buffer.WriteByte(input[pos])
 			for input[pos] != '"' {
+				buffer.WriteByte(input[pos])
+				pos++
 			}
 		}
+		buffer.WriteByte(input[pos])
+		key = buffer.String()
+		pos++
+
+		for input[pos] != ' ' {
+			pos++
+		}
+		if input[pos] == ':' {
+			pos++
+		} else {
+			return WrongSymbol
+		}
+
+		for input[pos] != ' ' {
+			pos++
+		}
+
+		if input[pos] == 'n' {
+			nilResolve(key)
+		} else if input[pos] == '"' {
+			stringResolve(key)
+		} else if input[pos] == '{' {
+			objectResolve(key)
+		} else if input[pos] == '[' {
+			arrayResolve(key)
+		} else if input[pos] >= '0' &&
+			input[pos] <= '9' && input[pos] == '-'{
+			numResolve(key)
+		} else if input[pos] == 't' && input[pos] == 'f' {
+			boolResolve(key)
+		} else {
+			return UnKnownValueTypeError
+		}
 	}
+	return nil
 }
 
-func object(input string) (int){
+func nilResolve(key string)  {
+	
+}
+
+func arrayResolve(key string)  {
+	
+}
+
+func boolResolve(key string)  {
+	
+}
+
+func numResolve(key string)  {
+	
+}
+func stringResolve(key string)  {
+
+}
+func objectResolve(input string) (int){
 	for input[pos] != '}' {
 		if input[pos] == '{' {
 			pos++
@@ -60,6 +128,14 @@ func object(input string) (int){
 		}
 	}
 	return 0
+}
+
+func next() error {
+	pos++
+	if pos >= length {
+		return OutOfBoundsError
+	}
+	return nil
 }
 
 func typeNull()  {
--- src/test.go
+++ src/test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	//"parser"
 	//"errors"
-	"bytes"
 )
 
 type test struct {
@@ -13,22 +12,25 @@ type test struct {
 }
 
 func main()  {
-	var buffer bytes.Buffer //Buffer是一个实现了读写方法的可变大小的字节缓冲
 
-	for {
-		if piece, ok := getNextString(); ok {
-			count++
-			/*
-			   func (b *Buffer) WriteString(s string) (n int, err error)
-			   Write将s的内容写入缓冲中，如必要会增加缓冲容量。返回值n为len(p)，err总是nil。如果缓冲变得太大，Write会采用错误值ErrTooLarge引发panic。
-			*/
-			buffer.WriteByte(piece)
-		} else {
-			break
-		}
-	}
+	fmt.Println('0')
 
-	fmt.Println("拼接后的结果为-->", buffer.String())
+	//var buffer bytes.Buffer //Buffer是一个实现了读写方法的可变大小的字节缓冲
+	//
+	//for {
+	//	if piece, ok := getNextString(); ok {
+	//		count++
+	//		/*
+	//		   func (b *Buffer) WriteString(s string) (n int, err error)
+	//		   Write将s的内容写入缓冲中，如必要会增加缓冲容量。返回值n为len(p)，err总是nil。如果缓冲变得太大，Write会采用错误值ErrTooLarge引发panic。
+	//		*/
+	//		buffer.WriteByte(piece)
+	//	} else {
+	//		break
+	//	}
+	//}
+	//
+	//fmt.Println("拼接后的结果为-->", buffer.String())
 }
 var count int = 0
 var teststr[] byte = []byte{'1','2','3'}
